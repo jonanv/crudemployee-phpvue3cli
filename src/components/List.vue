77102268cd3fe534074318a1a052832536e4f2d0
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-06-09 19:44:14 -0500

    Se hace la estructura de la tabla de empleados

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,13 +1,44 @@
 <template>
-    <div>
 
-        <div v-for="employee in employees" :key="employee.id_employee">
-            <div>{{ employee.id_employee }}</div>
-            <div>{{ employee.name }}</div>
-            <div>{{ employee.email }}</div>
+    <div class="container">
+
+        <div class="card">
+            <div class="card-header">
+                Employees
+            </div>
+                <div class="card-body">
+
+                <table class="table table-striped table-dark">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(employee) in employees" :key="employee.id_employee">
+                            <th scope="row">1</th>
+                            <td>{{ employee.id_employee }}</td>
+                            <td>{{ employee.name }}</td>
+                            <td>{{ employee.email }}</td>
+                            <td>
+                                <div class="btn-group" role="group" aria-label="Basic example">
+                                    <button type="button" class="btn btn-primary">Edit</button>
+                                    <button type="button" class="btn btn-danger">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
         </div>
         
     </div>
+
 </template>
 
 <script>
